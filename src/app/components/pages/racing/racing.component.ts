3afcb19f3d22f5d8d9c99ef831cd38094e548e58
Wernersbach, Markus <wernersbacher@gmail.com>
2025-04-11 16:12:17 +0200

    some racing component adjustments

--- src/app/components/pages/racing/racing.component.ts
+++ src/app/components/pages/racing/racing.component.ts
@@ -33,182 +33,198 @@ import { Subscription } from 'rxjs';
   ],
   template: `
     <div class="racing-component">
-      <h2>Race</h2>
+      <div class="flex flex-row flex-wrap gap-3">
+        <div class="w-full md:w-1/2 lg:w-1/3">
+          <div class="setup-section" class="" *ngIf="!isRacing">
+            <h3>Race Setup</h3>
+            <div class="flex flex-col gap-3">
+              <div>
+                <label for="vehicle-class">Vehicle Class:</label>
+                <p-dropdown
+                  id="vehicle-class"
+                  [options]="vehicleClasses"
+                  [(ngModel)]="selectedVehicleClass"
+                  placeholder="Select Vehicle Class"
+                  (ngModelChange)="onVehicleClassChange()"
+                  [disabled]="isRacing"
+                ></p-dropdown>
+              </div>
 
-      <div class="setup-section" *ngIf="!isRacing">
-        <h3>Race Setup</h3>
-        <div class="flex flex-col gap-3">
-          <div>
-            <label for="vehicle-class">Vehicle Class:</label>
-            <p-dropdown
-              id="vehicle-class"
-              [options]="vehicleClasses"
-              [(ngModel)]="selectedVehicleClass"
-              placeholder="Select Vehicle Class"
-              (ngModelChange)="onVehicleClassChange()"
-              [disabled]="isRacing"
-            ></p-dropdown>
+              <div *ngIf="selectedVehicleClass">
+                <h4>Select Track:</h4>
+                <p-table
+                  [value]="filteredTracks"
+                  [(selection)]="selectedTrack"
+                  selectionMode="single"
+                  [tableStyle]="{ 'min-width': '20rem', width: '100%' }"
+                >
+                  <ng-template pTemplate="header">
+                    <tr>
+                      <th>Name</th>
+                      <th>Country</th>
+                      <th>Difficulty</th>
+                    </tr>
+                  </ng-template>
+                  <ng-template pTemplate="body" let-track>
+                    <tr [pSelectableRow]="track">
+                      <td>{{ track.name }}</td>
+                      <td>{{ track.country }}</td>
+                      <td>{{ track.difficulty }}</td>
+                    </tr>
+                  </ng-template>
+                </p-table>
+              </div>
+
+              <div class="flex gap-4 mt-2">
+                <p-dropdown
+                  [(ngModel)]="numLaps"
+                  [options]="lapOptions"
+                  placeholder="Number of Laps"
+                  [disabled]="isRacing"
+                ></p-dropdown>
+
+                <p-dropdown
+                  [(ngModel)]="numOpponents"
+                  [options]="opponentOptions"
+                  placeholder="Number of Opponents"
+                  [disabled]="isRacing"
+                ></p-dropdown>
+              </div>
+
+              <div>
+                <p-button
+                  label="Start Race"
+                  icon="pi pi-flag"
+                  [disabled]="!canStartRace"
+                  (click)="startRace()"
+                ></p-button>
+              </div>
+            </div>
           </div>
+          <div class="race-section" *ngIf="isRacing">
+            <h3>Race in Progress</h3>
+            <div class="flex flex-col gap-3">
+              <p-card>
+                <p class="font-bold">
+                  {{ selectedTrack?.name }} - {{ selectedVehicleClass }}
+                </p>
+                <p>Lap: {{ getCurrentLap() }}</p>
+                <p>Time: {{ formatTime(raceState.currentTime) }}</p>
+              </p-card>
 
-          <div *ngIf="selectedVehicleClass">
-            <h4>Select Track:</h4>
+              <p-table
+                [value]="getRacePositions()"
+                [tableStyle]="{ 'min-width': '20rem', width: '100%' }"
+              >
+                <ng-template pTemplate="header">
+                  <tr>
+                    <th>Pos</th>
+                    <th>Driver</th>
+                    <th>Lap</th>
+                    <th>Last Lap</th>
+                    <th>Best Lap</th>
+                    <th>Gap</th>
+                    <th>Damage</th>
+                  </tr>
+                </ng-template>
+                <ng-template pTemplate="body" let-driver let-i="rowIndex">
+                  <tr [ngClass]="{ 'bg-yellow-100': driver.isPlayer }">
+                    <td>{{ i + 1 }}</td>
+                    <td>{{ driver.driver.name }}</td>
+                    <td>{{ driver.currentLap }}/{{ numLaps }}</td>
+                    <td>
+                      {{
+                        driver.lastLapTime
+                          ? formatTime(driver.lastLapTime)
+                          : '-'
+                      }}
+                    </td>
+                    <td>
+                      {{
+                        driver.bestLapTime
+                          ? formatTime(driver.bestLapTime)
+                          : '-'
+                      }}
+                    </td>
+                    <td>
+                      {{
+                        i === 0
+                          ? '-'
+                          : '+' + formatTimeDelta(driver.timeDeltaToAhead)
+                      }}
+                    </td>
+                    <td>
+                      {{
+                        driver.damage ? driver.damage.toFixed(2) + '%' : '0%'
+                      }}
+                    </td>
+                  </tr>
+                </ng-template>
+              </p-table>
+
+              <p-button
+                label="Cancel Race"
+                icon="pi pi-times"
+                (click)="cancelRace()"
+              ></p-button>
+            </div>
+          </div>
+          <div
+            class="results-section"
+            *ngIf="raceResults.length > 0 && !isRacing"
+          >
+            <h3>Race Results</h3>
             <p-table
-              [value]="filteredTracks"
-              [(selection)]="selectedTrack"
-              selectionMode="single"
+              [value]="raceResults"
               [tableStyle]="{ 'min-width': '20rem', width: '100%' }"
             >
               <ng-template pTemplate="header">
                 <tr>
-                  <th>Name</th>
-                  <th>Country</th>
-                  <th>Difficulty</th>
+                  <th>Position</th>
+                  <th>Driver</th>
+                  <th>Time</th>
+                  <th>Gap</th>
                 </tr>
               </ng-template>
-              <ng-template pTemplate="body" let-track>
-                <tr [pSelectableRow]="track">
-                  <td>{{ track.name }}</td>
-                  <td>{{ track.country }}</td>
-                  <td>{{ track.difficulty }}</td>
+              <ng-template pTemplate="body" let-result>
+                <tr [ngClass]="{ 'bg-yellow-100': isPlayerResult(result) }">
+                  <td>{{ result.position }}</td>
+                  <td>{{ result.driver.name }}</td>
+                  <td>{{ formatTime(result.totalTime) }}</td>
+                  <td>
+                    {{
+                      result.timeDelta
+                        ? '+' + formatTime(result.timeDelta)
+                        : '-'
+                    }}
+                  </td>
                 </tr>
               </ng-template>
             </p-table>
-          </div>
 
-          <div class="flex gap-4 mt-2">
-            <p-dropdown
-              [(ngModel)]="numLaps"
-              [options]="lapOptions"
-              placeholder="Number of Laps"
-              [disabled]="isRacing"
-            ></p-dropdown>
-
-            <p-dropdown
-              [(ngModel)]="numOpponents"
-              [options]="opponentOptions"
-              placeholder="Number of Opponents"
-              [disabled]="isRacing"
-            ></p-dropdown>
-          </div>
-
-          <div>
-            <p-button
-              label="Start Race"
-              icon="pi pi-flag"
-              [disabled]="!canStartRace"
-              (click)="startRace()"
-            ></p-button>
+            <div class="mt-3" *ngIf="raceResults.length > 0">
+              <p-button
+                label="Race Again"
+                icon="pi pi-refresh"
+                (click)="prepareNewRace()"
+              ></p-button>
+            </div>
           </div>
         </div>
-      </div>
 
-      <div class="race-section" *ngIf="isRacing">
-        <h3>Race in Progress</h3>
-        <div class="flex flex-col gap-3">
-          <p-card>
-            <p class="font-bold">
-              {{ selectedTrack?.name }} - {{ selectedVehicleClass }}
-            </p>
-            <p>Lap: {{ getCurrentLap() }}</p>
-            <p>Time: {{ formatTime(raceState.currentTime) }}</p>
+        <!-- racing log -->
+        <div class="w-fullmd:w-1/2 lg:w-1/3 ">
+          <h3>Race Log</h3>
+          <p-card class="max-h-[600px] overflow-scroll">
+            <p-scrollpanel
+              class="race-log mt-4"
+              *ngIf="raceResults.length > 0 || isRacing"
+            >
+              <p *ngFor="let log of getReversedLogs()">{{ log }}</p>
+            </p-scrollpanel>
           </p-card>
-
-          <p-table
-            [value]="getRacePositions()"
-            [tableStyle]="{ 'min-width': '20rem', width: '100%' }"
-          >
-            <ng-template pTemplate="header">
-              <tr>
-                <th>Pos</th>
-                <th>Driver</th>
-                <th>Lap</th>
-                <th>Last Lap</th>
-                <th>Best Lap</th>
-                <th>Gap</th>
-                <th>Damage</th>
-              </tr>
-            </ng-template>
-            <ng-template pTemplate="body" let-driver let-i="rowIndex">
-              <tr [ngClass]="{ 'bg-yellow-100': driver.isPlayer }">
-                <td>{{ i + 1 }}</td>
-                <td>{{ driver.driver.name }}</td>
-                <td>{{ driver.currentLap }}/{{ numLaps }}</td>
-                <td>
-                  {{
-                    driver.lastLapTime ? formatTime(driver.lastLapTime) : '-'
-                  }}
-                </td>
-                <td>
-                  {{
-                    driver.bestLapTime ? formatTime(driver.bestLapTime) : '-'
-                  }}
-                </td>
-                <td>
-                  {{
-                    i === 0
-                      ? '-'
-                      : '+' + formatTimeDelta(driver.timeDeltaToAhead)
-                  }}
-                </td>
-                <td>
-                  {{ driver.damage ? driver.damage.toFixed(2) + '%' : '0%' }}
-                </td>
-              </tr>
-            </ng-template>
-          </p-table>
-
-          <p-button
-            label="Cancel Race"
-            icon="pi pi-times"
-            (click)="cancelRace()"
-          ></p-button>
         </div>
       </div>
-
-      <div class="results-section" *ngIf="raceResults.length > 0 && !isRacing">
-        <h3>Race Results</h3>
-        <p-table
-          [value]="raceResults"
-          [tableStyle]="{ 'min-width': '20rem', width: '100%' }"
-        >
-          <ng-template pTemplate="header">
-            <tr>
-              <th>Position</th>
-              <th>Driver</th>
-              <th>Time</th>
-              <th>Gap</th>
-            </tr>
-          </ng-template>
-          <ng-template pTemplate="body" let-result>
-            <tr [ngClass]="{ 'bg-yellow-100': isPlayerResult(result) }">
-              <td>{{ result.position }}</td>
-              <td>{{ result.driver.name }}</td>
-              <td>{{ formatTime(result.totalTime) }}</td>
-              <td>
-                {{
-                  result.timeDelta ? '+' + formatTime(result.timeDelta) : '-'
-                }}
-              </td>
-            </tr>
-          </ng-template>
-        </p-table>
-
-        <div class="mt-3" *ngIf="raceResults.length > 0">
-          <p-button
-            label="Race Again"
-            icon="pi pi-refresh"
-            (click)="prepareNewRace()"
-          ></p-button>
-        </div>
-      </div>
-      <p-card>
-        <p-scrollpanel
-          class="race-log mt-4"
-          *ngIf="raceResults.length > 0 || isRacing"
-        >
-          <p *ngFor="let log of getReversedLogs()">{{ log }}</p>
-        </p-scrollpanel>
-      </p-card>
     </div>
   `,
   styles: [
@@ -228,7 +244,8 @@ export class RacingComponent implements OnInit, OnDestroy {
   filteredTracks: Track[] = [];
   vehicleClasses = Object.values(VehicleClass);
   selectedTrack: Track | null = null;
-  selectedVehicleClass: VehicleClass | null = null;
+  selectedVehicleClass: VehicleClass = VehicleClass.GT3;
+  currentRaceConfig: RaceConfig | null = null;
 
   numLaps: number = 3;
   numOpponents: number = 5;
@@ -311,7 +328,7 @@ export class RacingComponent implements OnInit, OnDestroy {
   startRace(): void {
     if (!this.canStartRace) return;
 
-    const config: RaceConfig = {
+    this.currentRaceConfig = {
       track: this.selectedTrack!,
       vehicleClass: this.selectedVehicleClass!,
       numLaps: this.numLaps,
@@ -320,7 +337,7 @@ export class RacingComponent implements OnInit, OnDestroy {
     };
 
     this.raceResults = []; // Clear previous results
-    this.raceService.startRace(config);
+    this.raceService.startRace(this.currentRaceConfig);
     this.isRacing = true;
   }
 
@@ -336,8 +353,12 @@ export class RacingComponent implements OnInit, OnDestroy {
   getRacePositions(): any[] {
     return [...this.raceState.positions].sort((a, b) => {
       // Sort by total progress (lap * track length + position)
-      const progressA = a.currentLap * 1000 + a.trackPosition; // assuming track length is around 1000
-      const progressB = b.currentLap * 1000 + b.trackPosition;
+      const progressA =
+        a.currentLap * this.currentRaceConfig?.track.lengthMeters! +
+        a.trackPosition;
+      const progressB =
+        b.currentLap * this.currentRaceConfig?.track.lengthMeters! +
+        b.trackPosition;
       return progressB - progressA; // descending order
     });
   }
